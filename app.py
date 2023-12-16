2116be22a6dd92b204b64474f373f2e3cbf2613b
Aryan ROUT <routa2@kgv.hk>
2023-12-16 12:46:32 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,74 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from twilio.rest import Client
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+# Define Verify_otp() function
+@app.route('/login' , methods=['POST'])
+def verify_otp():
+    username = request.form['username']
+    password = request.form['password']
+    mobile_number = request.form['number']
+
+    if username == 'verify' and password == '12345':   
+        account_sid = 'AC72cc36daa180c96100224599079ab999'
+        auth_token = '44287bc86a50acf716b11d5cbc99f53f'
+        client = Client(account_sid, auth_token)
+
+        verification = client.verify \
+            .services('VAd8aa4f4cb1a68669e95cea3c68108a81') \
+            .verifications \
+            .create(to=mobile_number, channel='sms')
+
+        print(verification.status)
+        return render_template('otp_verify.html')
+    else:
+        return render_template('user_error.html')
+
+
+
+@app.route('/otp', methods=['POST'])
+def get_otp():
+    print("Processing")
+    received_otp = request.form['received_otp']
+    mobile_number = request.form['number']
+
+    account_sid = 'AC72cc36daa180c96100224599079ab999'
+    auth_token = '44287bc86a50acf716b11d5cbc99f53f'
+
+    client = Client(account_sid, auth_token)
+
+
+
+    verification_check = client.verify \
+        .services('VAd8aa4f4cb1a68669e95cea3c68108a81') \
+        .verification_checks \
+        .create(to=mobile_number, code=received_otp)
+    print(verification_check.status)
+
+    if verification_check.status == 'pending':
+        return "Entered OTP is wrong"
+    else:
+        return redirect('https://collaborative-document-nw32.onrender.com')
+    
+
+   
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
+
